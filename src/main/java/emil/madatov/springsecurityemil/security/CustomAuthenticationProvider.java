a25dbaa35500d28f0b5032748d4ea06b9d34b80d
e.madatov <e.madatov@bftcom.com>
2023-01-29 17:55:24 +0300

    chapter 2 end

---
+++ src/main/java/emil/madatov/springsecurityemil/security/CustomAuthenticationProvider.java
@@ -0,0 +1,29 @@
+package emil.madatov.springsecurityemil.security;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if ("emil".equals(username) && "1234".equals(password)) {
+             return new UsernamePasswordAuthenticationToken(username, password, List.of());
+        } else throw new AuthenticationCredentialsNotFoundException("Credentials not found!");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class
+                .isAssignableFrom(authentication);
+    }
+}
---
+++ src/main/java/emil/madatov/springsecurityemil/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package emil.madatov.springsecurityemil.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
+}
